d6b34b2acef3c2ffef792252c6a8b39645819b7b
Nik-olaso <122684717+Nik-olaso@users.noreply.github.com>
2024-08-09 12:56:02 +0300

    Сделал переходы на страницы

--- render_website.py
+++ render_website.py
@@ -17,22 +17,29 @@ def on_reload():
     pages_path = "car_pages"
     os.makedirs(pages_path, exist_ok=True)
 
-    
+    marks = []
+    car_files = []
     for root, dirs, files in os.walk('cars'):
+        for dir in dirs:
+            marks.append(dir)
         for file in files:
-            if file.endswith('.json'):
-                cars_file = codecs.open(f"{root}/{file}", "r", "utf_8_sig")
-                cars_params = cars_file.read()
-                cars_file.close()
-                cars_params = json.loads(cars_params)
-                filename, suffix = os.path.splitext(file)
-                rendered_page = template.render(
-                    cars=cars_params,
-                )
+            car_files.append(file)
+    for page_number, car_file in enumerate(car_files):
+        if car_file.endswith('.json'):
+            path, suffix = os.path.splitext(car_file)
+            car_info = codecs.open(f"cars/{path}/{car_file}", "r", "utf_8_sig")
+            cars_params = car_info.read()
+            car_info.close()
+            cars_params = json.loads(cars_params)
+            filename, suffix = os.path.splitext(car_file)
+            rendered_page = template.render(
+                page_number=page_number,
+                marks=marks,
+                cars=cars_params,
+            )
 
-                with open(f"{pages_path}/{filename}.html", 'w', encoding="utf8") as file:
-                    file.write(rendered_page)
-
+            with open(f"{pages_path}/{filename}.html", 'w', encoding="utf8") as file:
+                file.write(rendered_page)
     print("Site rebuilt")
 
 on_reload()
