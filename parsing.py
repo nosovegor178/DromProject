5043d90ebcdf8fb8a63399ad08d7aa9c8ea8461d
nosovegor178 <nosovbelakovegor01@gmail.com>
2024-08-11 18:34:47 +0300

    Сделал пагинацию, исправил ошибку* и изменил список марок
    *грузились одинаковые картинки
    
    Ещё я удалил папку cars из-за очень большого количества файлов внутри

--- parsing.py
+++ parsing.py
@@ -2,6 +2,7 @@ import requests
 import os
 from bs4 import BeautifulSoup
 from pathvalidate import sanitize_filename
+from itertools import count
 import json
 
 
@@ -15,6 +16,7 @@ def download_car_cards(car_cards, filename, folder):
 
 def download_image(car_kind, filename, image_url):
     sanitized_filename = sanitize_filename(filename)
+    os.makedirs(car_kind, exist_ok=True)
     os.chdir(car_kind)
     os.makedirs('media', exist_ok=True)
     path_to_file = os.path.join('media', sanitized_filename)
@@ -28,46 +30,50 @@ def download_image(car_kind, filename, image_url):
 def parse_car_cards(car_kind):
     parsed_cards = []
     image_urls = []
-    url = f'https://spb.drom.ru/{car_kind}/all/'
     params = {
         'ph': 1,
         'unsold': 1,
         'mv': 0.7
     }
-    response = requests.get(url, params=params)
-    response.raise_for_status()
-    soup = BeautifulSoup(response.text, 'lxml')
-    car_cards = soup.find('div', class_='css-1nvf6xk ejck0o60').find_all('div', attrs={'data-ftid': 'bulls-list_bull'})
-    for car_card in car_cards:
-        title = car_card.find('h3', class_='css-16kqa8y efwtv890').text.split(',')
-        auto_info = car_card.find('div', class_='css-1fe6w6s e162wx9x0').text.split(',')
-        engine_power_info = auto_info[0].split('(')
-        parsed_card = {
-                'model': title[0],
-                'year': title[1],
-                'price': car_card.find('span', class_="css-46itwz e162wx9x0").text,
-                'horsepower': engine_power_info[1][:-1],
-                'engine_info': f'{engine_power_info[0]}, {auto_info[1]}',
-                'transmission': auto_info[2],
-                'drive': auto_info[3],
-                }
-        try:
-            parsed_card['mileage'] = auto_info[4]
-        except IndexError:
-            print('Без пробега')
-        parsed_cards.append(parsed_card)
-        image_urls.append(soup.find('div', class_='css-10vnevh e1pqv6mt0').find(
-            'div', class_='css-aqyz46 e1e9ee560').find('img')['src'])
+    for page_number in count(1):
+        url = f'https://spb.drom.ru/{car_kind}/all/page{page_number}'
+        response = requests.get(url, params=params)
+        response.raise_for_status()
+        soup = BeautifulSoup(response.text, 'lxml')
+        if not soup.find('div', attrs={'data-bulletin-list': 'true'}):
+            break
+        else:
+            car_cards = soup.find('div', attrs={'data-bulletin-list': 'true'}).find_all('div', attrs={'data-ftid': 'bulls-list_bull'})
+            for car_card in car_cards:
+                title = car_card.find('h3', class_='css-16kqa8y efwtv890').text.split(',')
+                auto_info = car_card.find('div', class_='css-1fe6w6s e162wx9x0').text.split(',')
+                engine_power_info = auto_info[0].split('(')
+                parsed_card = {
+                        'model': title[0],
+                        'year': title[1],
+                        'price': car_card.find('span', class_="css-46itwz e162wx9x0").text,
+                        'horsepower': engine_power_info[1][:-1],
+                        'engine_info': f'{engine_power_info[0]}, {auto_info[1]}',
+                        'transmission': auto_info[2],
+                        'drive': auto_info[3],
+                        }
+                try:
+                    parsed_card['mileage'] = auto_info[4]
+                except IndexError:
+                    print('Без пробега')
+                parsed_cards.append(parsed_card)
+                image_urls.append(car_card.find('div', attrs={'data-ftid': 'bull_image'}).find(
+                    'div', attrs={'data-first-photo': 'true'}).find(
+                    'div', class_='css-aqyz46 e1e9ee560').find('img')['src'])
     return parsed_cards, image_urls
 
 
 if __name__ == '__main__':
-    car_kinds = ['acura', 'baic', 'bmw', 'chevrolet', 'ford', 'honda']
+    car_kinds = ['acura', 'baic', 'citroen', 'belgee', 'porsche', 'jaguar']
     os.makedirs('cars', exist_ok=True)
     os.chdir('cars')
     for car_kind in car_kinds:
         parsed_cards = parse_car_cards(car_kind)[0]
-        print(len(parsed_cards))
         download_car_cards(parsed_cards, f'{car_kind}.json', car_kind)
         for image_number, image_url in enumerate(parse_car_cards(car_kind)[1]):
-            download_image(car_kind, f'{car_kind}_{image_number}.png', image_url)
+            download_image(car_kind, f'{car_kind}_{image_number+1}.png', image_url)
